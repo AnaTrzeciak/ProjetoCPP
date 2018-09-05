7924c5c90bbc843224617f3c32c7afd25681cd36
AnaTrzeciak <43004395+AnaTrzeciak@users.noreply.github.com>
2018-09-05 18:01:24 -0300

    Create var.cpp
    Variaves

---
+++ var.cpp
@@ -0,0 +1,21 @@
+//Variaveis: 
+//int  inteiros
+//char caracter individual
+//double numeros nao inteiros
+//string conjunto de caracter
+//bool variaveis logicas
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   string nome;
+   int idade;
+
+   cout<< "digite seu nome \n";
+   cin>> nome;
+   cout<< "Agora, digite sua idade \n";
+   cin>> idade;
+   cout<< "Ola "<<nome<<", sua idade é "<<idade<<"! \n";
+} 
