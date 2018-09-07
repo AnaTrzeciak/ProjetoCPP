d6b451f480284da680f053269cea6f96d2e1036d
AnaTrzeciak <amgtrzeciak@gmail.com>
2018-09-07 14:14:26 -0300

    mudança var.cpp

--- var.cpp
+++ var.cpp
@@ -1,4 +1,4 @@
-//Variaveis: 
+//Programming Principles and Practice Using C++: Secão 3.3 Input and Type
 //int  inteiros
 //char caracter individual
 //double numeros nao inteiros
@@ -8,6 +8,7 @@
 #include<iostream>
 using namespace std;
 
+/*
 int main()
 {
    string nome;
@@ -18,4 +19,16 @@ int main()
    cout<< "Agora, digite sua idade \n";
    cin>> idade;
    cout<< "Ola "<<nome<<", sua idade é "<<idade<<"! \n";
+}*/
+//a string só le palavras antes do espaço, espaço em branco nao le 
+//o programa abaixo le duas palavras
+
+int main()
+{
+    string nome1,nome2;
+    int idade;
+ 
+    cout<< "Digite seu nome, sobrenome e idade\n";
+    cin>> nome1>>nome2>>idade;      //leia 2 string e um inteiro
+    cout<< "Olá, "<<nome1<<" "<<nome2<<", sua idade é "<<idade<<" anos. \n";
 } 
