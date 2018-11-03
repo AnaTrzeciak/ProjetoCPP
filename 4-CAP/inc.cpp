427599af3f671ca6b8f846df1c6d1dee9b6e1fa0
AnaTrzeciak <amgtrzeciak@gmail.com>
2018-11-02 21:04:13 -0300

    adição de arquivos

---
+++ 4-CAP/inc.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main(){
+
+ int a;
+
+ cout<<"Digite um numero \n";
+ cin>>a;
+
+// a=++a;  //incrementa 1
+   a=a+5;  //incrementa 5
+ 
+ cout<<"O resultado é "<<a<<"\n\n";
+} 
