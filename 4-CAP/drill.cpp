560f070b7e19c110112c51a38391920c743d6df6
AnaTrzeciak <amgtrzeciak@gmail.com>
2018-11-13 22:58:11 -0200

    adicao de arquivos

---
+++ 4-CAP/drill.cpp
@@ -0,0 +1,31 @@
+//Programming Principles and Practice Using C++: Cap 4-Computation 
+//Exercicios Drill
+
+#include"/home/anatrzeciak/Documentos/GitHub/ProjetoCPP/std_lib_facilities.h"
+
+//Programa 1
+//int main(){
+
+//int a,b;
+//cout<<"Digite dois inteiros \n";
+
+//while(cin>>a>>b)
+//	cout<<a<<","<<b<<"\n";
+//}
+
+//=====================================================
+
+//Programa 2
+
+int main(){
+
+int a,b;
+
+cout<<"Digite dois inteiros\n";
+while(cin>>a>>b)
+	if(a>b)
+	cout<<"o maior valor é "<<a<<"\n";
+	else
+	cout<<"o maior valor é "<<b<<"\n";
+}
+
---
+++ 4-CAP/exer2.cpp
@@ -0,0 +1,13 @@
+//Programming Principles and Practice Using C++: Cap 4-Computation 
+
+#include"/home/anatrzeciak/Documentos/GitHub/ProjetoCPP/std_lib_facilities.h"
+
+int main(){
+
+vector<double>distacia;
+
+cout<<"Digite as distancias entre as cidades \n";
+	distancia.push_back(distancia);
+
+for(int i;i<distancia.size();++i)
+	cout<<"A distancia total entre as cidades é "<<
